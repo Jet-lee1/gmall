4e3ad7f3e97f7cb630ee7f353fea74e979a8ca57
lizhong <yx@qq.com>
2020-03-24 17:43:26 +0800

    第二天结束

--- gmall-user-web/src/main/java/com/atguigu/gmall/user/controller/UserController.java
+++ gmall-user-web/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -1,10 +1,10 @@
 package com.atguigu.gmall.user.controller;
 
 
-import com.atguigu.gmall.user.bean.UmsMember;
-import com.atguigu.gmall.user.bean.UmsMemberReceiveAddress;
-import com.atguigu.gmall.user.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.atguigu.gmall.bean.UmsMember;
+import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,7 +14,7 @@ import java.util.List;
 @Controller
 public class UserController {
 
-    @Autowired
+    @Reference
     UserService userService;
 
     @ResponseBody
