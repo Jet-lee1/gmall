9a49a9ad474998bb0d2c6ed1ad93045020051a84
lizhong <yx@qq.com>
2020-05-26 22:51:00 +0800

    es

---
+++ gmall-search-service/src/test/java/com/atguigu/gmall/search/GmallSearchServiceApplicationTests.java
@@ -0,0 +1,30 @@
+package com.atguigu.gmall.search;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.atguigu.gmall.service.SkuService;
+import io.searchbox.client.JestClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GmallSearchServiceApplicationTests {
+
+	@Reference
+	SkuService skuService;
+
+	@Autowired
+	JestClient jestClient;
+
+	@Test
+	public void contextLoads() throws IOException{
+		jestClient.execute(null);
+	}
+
+}
