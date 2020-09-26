7ecc6312e2785387919b598d1a21bb0306f80bec
lizhong <yx@qq.com>
2020-09-26 11:46:15 +0800

    保存用户  用户名+密码 保证唯一性

--- gmall-user-service/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
+++ gmall-user-service/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
@@ -1,12 +1,16 @@
 package com.atguigu.gmall.user.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
+import com.alibaba.fastjson.JSON;
 import com.atguigu.gmall.bean.UmsMember;
 import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
 import com.atguigu.gmall.service.UserService;
 import com.atguigu.gmall.user.mapper.UmsMemberReceiveAddressMapper;
 import com.atguigu.gmall.user.mapper.UserMapper;
+import com.atguigu.gmall.util.RedisUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import redis.clients.jedis.Jedis;
 
 import java.util.List;
 
@@ -18,6 +22,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     UmsMemberReceiveAddressMapper umsMemberReceiveAddressMapper;
+
+    @Autowired
+    RedisUtil redisUtil;
 
     @Override
     public List<UmsMember> getAllUser() {
@@ -39,5 +46,61 @@ public class UserServiceImpl implements UserService {
 //        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = umsMemberReceiveAddressMapper.selectByExample(example);
 
         return umsMemberReceiveAddresses;
+    }
+
+    @Override
+    public UmsMember login(UmsMember umsMember) {
+        Jedis jedis = null;
+        try {
+            jedis = redisUtil.getJedis();
+
+            if(jedis!=null){
+                String umsMemberStr = jedis.get("user:" + umsMember.getPassword() + ":info");
+
+                if (StringUtils.isNotBlank(umsMemberStr)) {
+                    // 密码正确
+                    UmsMember umsMemberFromCache = JSON.parseObject(umsMemberStr, UmsMember.class);
+                    return umsMemberFromCache;
+                }
+            }
+            // 链接redis失败，开启数据库
+            UmsMember umsMemberFromDb =loginFromDb(umsMember);
+            if(umsMemberFromDb!=null){
+                jedis.setex("user:" + umsMember.getPassword()+umsMember.getUsername() + ":info",60*60*24, JSON.toJSONString(umsMemberFromDb));
+            }
+            return umsMemberFromDb;
+        }finally {
+            jedis.close();
+        }
+    }
+
+    @Override
+    public void addUserToken(String token, String memberId) {
+        Jedis jedis = redisUtil.getJedis();
+        jedis.setex("user:"+memberId+":token",60*60*2,token);
+        jedis.close();
+    }
+
+    @Override
+    public void addOauthUser(UmsMember umsMember) {
+        userMapper.insertSelective(umsMember);
+    }
+
+    @Override
+    public UmsMember checkOauthUser(UmsMember umsCheck) {
+        UmsMember umsMember = userMapper.selectOne(umsCheck);
+        return umsMember;
+    }
+
+    private UmsMember loginFromDb(UmsMember umsMember) {
+
+        List<UmsMember> umsMembers = userMapper.select(umsMember);
+
+        if(umsMembers!=null){
+            return umsMembers.get(0);
+        }
+
+        return null;
+
     }
 }
